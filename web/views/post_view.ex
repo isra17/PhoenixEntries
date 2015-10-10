c1c1d4078d4be60c5094b9b6ae7fed6cd58818f0
isra17 <isra017@gmail.com>
2015-10-10 16:49:41 -0400

    Add markdown support

--- web/views/post_view.ex
+++ web/views/post_view.ex
@@ -1,3 +1,8 @@
 defmodule PhoenixEntries.PostView do
   use PhoenixEntries.Web, :view
+
+  def html_content(content) do
+    safe_content = Phoenix.HTML.Safe.to_iodata(content)
+    {:safe, Earmark.to_html(safe_content)}
+  end
 end
