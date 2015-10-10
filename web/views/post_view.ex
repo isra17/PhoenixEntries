97db1357d3eeb725d4c42b044cbf112e2112b78c
isra17 <isra017@gmail.com>
2015-10-10 18:39:06 -0400

    Doesn't escape markdown html

--- web/views/post_view.ex
+++ web/views/post_view.ex
@@ -2,7 +2,6 @@ defmodule PhoenixEntries.PostView do
   use PhoenixEntries.Web, :view
 
   def html_content(content) do
-    safe_content = Phoenix.HTML.Safe.to_iodata(content)
-    {:safe, Earmark.to_html(safe_content)}
+    {:safe, Earmark.to_html(content)}
   end
 end
